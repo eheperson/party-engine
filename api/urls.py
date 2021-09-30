19d66d38989ee716b4ed6b7fa59200f4744f19a8
eheperson <barannkuzu@gmail.com>
2021-09-30 13:40:29 +0300

    Room Created with Success

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.urls.conf import include
-from .views import main
+from .views import main, CreateRoomView
 from .views import RoomView
 
 urlpatterns = [
     path('main', main, name='root'),
     path('room', RoomView.as_view(), name='Room View'),
+    path('create-room', CreateRoomView.as_view(), name="Create Room"),
 ]
