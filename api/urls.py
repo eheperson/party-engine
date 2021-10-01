2900404a29708bd43126f885b6f7d9e84e13889c
eheperson <barannkuzu@gmail.com>
2021-10-01 10:18:46 +0300

    Creating The Room Join Page

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.urls.conf import include
 from .views import main, CreateRoomView
-from .views import RoomView, GetRoom
+from .views import RoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     path('main', main, name='root'),
     path('room', RoomView.as_view(), name='Room View'),
     path('create-room', CreateRoomView.as_view(), name="Create Room"),
     path('get-room', GetRoom.as_view(), name="Get Room"), # http://127.0.0.1:8000/api/get-room?code=LEENAC
+    path('join-room', JoinRoom.as_view())
 ]
