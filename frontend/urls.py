d25e0ebe79863c57716da3bd66b4d2bf1c712e01
eheperson <barannkuzu@gmail.com>
2021-10-04 16:22:12 +0300

    Functional Components (useState, useEffect) - applied with info page

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,7 @@ app_name = 'frontend'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('info', index),
     path('join', index, name='join to room'),
     path('create', index, name='create a room'),
     path('room/<str:roomCode>', index, name='room'),
