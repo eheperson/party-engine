6124024dad66922c0f3aeebe03941e5129089f4c
eheperson <barannkuzu@gmail.com>
2021-10-04 11:58:20 +0300

    Using the spotify API

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID = "08cb230a331944c7bfa503dfc73dd46c" #obtain it from spotify
-CLIENT_SECRET = "f78338ef85e74cf2be80970a90406b69" #obtain it from spotify
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect" 
+CLIENT_ID = "77410248cee7469886ec0ea0e25c52e0" #obtain it from spotify
+CLIENT_SECRET = "05e41ba462444b2e97add5f627340231" #obtain it from spotify
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
