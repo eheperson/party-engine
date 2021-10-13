27a586718ba31186457e4075824a57f2fc314bd1
eheperson <barannkuzu@gmail.com>
2021-10-13 18:34:37 +0300

    project is done

--- musiccontroller/settings.py
+++ musiccontroller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$3tgu3#!_!_v6r+)5e3_m^7#lwa5o2a2&y0+=ptg@c%&sb0uci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "77410248cee7469886ec0ea0e25c52e0" #obtain it from spotify
 CLIENT_SECRET = "05e41ba462444b2e97add5f627340231" #obtain it from spotify
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "http://127.0.0.1:8013/spotify/redirect"
